12bf8cbbb00569fe3f1fa0d01e213ebc0150d87f
Владимир <PePPeRmix@users.noreply.github.com>
2018-02-15 09:26:43 +0300

    Update rpcmining.cpp

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -642,12 +642,13 @@ Value getblocktemplate(const Array& params, bool fHelp)
     result.push_back(Pair("votes", aVotes));
 
 
+    UniValue masternodeObj(UniValue::VOBJ);
     if (pblock->payee != CScript()) {
         CTxDestination address1;
         ExtractDestination(pblock->payee, address1);
         CBitcoinAddress address2(address1);
-        result.push_back(Pair("payee", address2.ToString().c_str()));
-        result.push_back(Pair("payee_amount", (int64_t)pblock->vtx[0].vout[1].nValue));
+        masternodeObj.push_back(Pair("payee", address2.ToString().c_str()));
+        masternodeObj.push_back(Pair("amount", (int64_t)pblock->vtx[0].vout[1].nValue));
     } else {
         result.push_back(Pair("payee", ""));
         result.push_back(Pair("payee_amount", ""));
@@ -655,6 +656,8 @@ Value getblocktemplate(const Array& params, bool fHelp)
 
     result.push_back(Pair("masternode_payments", pblock->nTime > Params().StartMasternodePayments()));
     result.push_back(Pair("enforce_masternode_payments", true));
+    result.push_back(Pair("masternode_payments_enforced", true));
+    result.push_back(Pair("superblocks_enabled", false));
 
     return result;
 }
